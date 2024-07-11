2e5b0fc35e0bc916bed5a73e5e17a48836067376
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-11 14:36:54 +0100

    completed ex 3.1-3.3

---
+++ fullstack-open-exercises/README.md
@@ -0,0 +1,3 @@
+I made a read me
+
+practicing attaching a local file to a remote. Both were empty. Next want to try attach an existing project to an empty repo.
---
+++ fullstack-open-exercises/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const phonebookData = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+// const phonebookData = require("./data/phonebookData.js");
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebookData);
+});
+
+app.get("/api/info", (request, response) => {
+  requestReceivedAt = Date.now();
+  formattedReceviedAt = new Date(requestReceivedAt);
+  response.send(`<p>Phonebook has info for ${phonebookData.length} people<p>
+    <p>${formattedReceviedAt}</p>`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = phonebookData.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// app.delete("/api/notes/:id", (request, response) => {
+//   const id = request.params.id;
+//   notes = notes.filter((note) => note.id !== id);
+
+//   response.status(204).end();
+// });
+
+// app.post("/api/notes", (request, response) => {
+//   const body = request.body;
+
+//   if (!body.content) {
+//     return response.status(400).json({
+//       error: "content missing",
+//     });
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: Boolean(body.important) || false,
+//     id: generateId(),
+//   };
+
+//   notes = notes.concat(note);
+
+//   response.json(note);
+// });
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
